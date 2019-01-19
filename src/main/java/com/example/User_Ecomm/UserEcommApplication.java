c39ce710b44580b50c71b0f6e407b142fc2484c8
akshita <akshim2002@gmail.com>
2019-01-19 10:48:09 +0530

    first commit

---
+++ src/main/java/com/example/User_Ecomm/UserEcommApplication.java
@@ -0,0 +1,14 @@
+package com.example.User_Ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEcommApplication.class, args);
+	}
+
+}
+
